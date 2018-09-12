a9be61bfc23aae80c020db52b199999ea853f968
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2018-09-12 09:58:09 -0400

    Added runningdb to environment vars so that it can be configured

--- libraries/lambda_handlers/instance_handler.py
+++ libraries/lambda_handlers/instance_handler.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from __future__ import unicode_literals, print_function
-import json
+import os
 from abc import ABCMeta
 from libraries.lambda_handlers.handler import Handler
 from libraries.tools.lambda_utils import is_lambda_running, set_lambda_running, lambda_min_remaining
@@ -17,10 +17,7 @@ class InstanceHandler(Handler):
     def run(self, **kwargs):
         # check if already running
 
-        self.logger.warning(json.dumps(kwargs))
-        event = kwargs['event']
-        env_vars = self.retrieve(event, 'stage-variables', 'payload')
-        running_db_name = self.retrieve_with_default(env_vars, 'running_db', '{}d43-catalog-running'.format(self.stage_prefix()))
+        running_db_name = os.environ.get('RUNNINGDB')
 
         if is_lambda_running(self.context, running_db_name):
             min_remaining = lambda_min_remaining(self.context)
